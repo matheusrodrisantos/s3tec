4bb1eb9bebbf6a23439942ccb6432aebf9cf6fd5
Matheus Rodrigues <matheus.rodrisantos@outlook.com>
2022-11-16 18:28:28 -0300

    classe de contato da s3tec

---
+++ Model/Contato.cs
@@ -0,0 +1,11 @@
+namespace s3tec.Model;
+
+public class Contato
+{
+    public int Id; 
+    public string Nome;
+    public string Email;
+    public string Telefone; 
+    public string  Mensagem; 
+
+}
